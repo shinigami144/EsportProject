a64b37215d9e916e095dff7cf23dd5a720a2096e
Shini <d.abraham@ludus-academie.com>
2019-11-04 12:15:04 +0100

    Creation de Unite
    Base de Unite

--- EsportGDProjet/Assets/Script/ClassUnite.cs
+++ EsportGDProjet/Assets/Script/ClassUnite.cs
@@ -2,17 +2,112 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClassUnite : MonoBehaviour
+public class ClassUnite
 {
-    // Start is called before the first frame update
-    void Start()
+    private int pointDeVie;
+    private int maxPointDeVie;
+    private string nom;
+    private GameObject cible;
+    private int attaque;
+    private int defence;
+    private int mouvementVitesse;
+
+
+    // GETTEUR
+    public int GetPointDeVie()
     {
-        
+        return pointDeVie;
     }
 
-    // Update is called once per frame
-    void Update()
+    public int GetMaxPointDeVie()
     {
-        
+        return maxPointDeVie;
     }
+
+    public string GetNom()
+    {
+        return nom;
+    }
+
+    public GameObject GetCible()
+    {
+        return cible;
+    }
+
+    public int GetAttaque()
+    {
+        return attaque;
+    }
+
+    public int GetDefence()
+    {
+        return defence;
+    }
+    
+    public int GetMouvemenetVitesse()
+    {
+        return mouvementVitesse;
+    }
+    //-------//
+    // SETTEUR
+
+    public void SetPointDeVie(int nouveauPointDeVie)
+    {
+        pointDeVie = nouveauPointDeVie;
+    }
+
+    public void SetMaxPointDeVie(int nouveauMaxPointDeVie)
+    {
+        maxPointDeVie = nouveauMaxPointDeVie;
+    }
+
+    public void SetNom(string nouveauNom)
+    {
+        nom = nouveauNom;
+    }
+
+    public void SetCible(GameObject theCible)
+    {
+        cible = theCible;
+    }
+
+    public void SetAttaque(int nouvelleAttaque)
+    {
+        attaque = nouvelleAttaque;
+    }
+
+    public void SetDefence(int nouvelleDefence)
+    {
+        defence = nouvelleDefence;
+    }
+
+    public void SetMouvementVitesse(int mouvementVitesse)
+    {
+        this.mouvementVitesse = mouvementVitesse;
+    }
+
+    //-----//
+    // Constructeur
+
+    ClassUnite() : this("Inconu",0,0,0,0)
+    {
+
+    }
+
+    ClassUnite(string _nom,int _MaxPointDeVie,int _attaque,int _defence,int _mouvementVitesse)
+    {
+        nom = _nom;
+        maxPointDeVie = _MaxPointDeVie;
+        attaque = _attaque;
+        defence = _defence;
+        mouvementVitesse = _mouvementVitesse;
+        cible = null;
+    }
+
+    ClassUnite(string _nom) : this(_nom, 0, 0, 0, 0)
+    {
+
+    }
+    // ----- // 
+
 }
